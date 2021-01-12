428d39192a252455a07e7219691ab8a167d4b98f
Jesse-Lucas1996 <61496789+Jesse-Lucas1996@users.noreply.github.com>
2021-01-12 13:46:01 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import pic from './Jesse.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> Jesse Lucas
+        <img src={pic} />
+        <p>
+        Here is a bio about me! here are my skills and what i do in life. 
+        </p>
+        <a
+         
+        >
+        Project 1 
+        </a>
+        <a
+        >
+          Project 2
+        </a>
+
+        <a
+        >
+        Project 3
+        </a>
+      </header>
+
+      <footer> 
+      <ul>
+      <li> https://github.com/Jesse-Lucas1996  </li>
+      </ul>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
